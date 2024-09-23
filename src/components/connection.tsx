22e19d1e5e840cd1205b436fcb06805f6afab4e8
Grace Xin <qingqinggrace@gmail.com>
2024-09-23 18:00:34 -0400

    add auth and change name to tale

--- src/components/connection.tsx
+++ src/components/connection.tsx
@@ -1,22 +1,27 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from 'firebase/app'
-import { getFirestore, Firestore } from 'firebase/firestore'
+import { FirebaseApp, initializeApp } from 'firebase/app'
+import { Firestore, getFirestore } from 'firebase/firestore'
+import { Auth, getAuth } from "firebase/auth";
+import firebaseui from 'firebaseui';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyAQRWkV63etf7t2GClB_U-XvEs2SdyJYKE',
-  authDomain: 'splash-7e99f.firebaseapp.com',
-  projectId: 'splash-7e99f',
-  storageBucket: 'splash-7e99f.appspot.com',
-  messagingSenderId: '751418611409',
-  appId: '1:751418611409:web:ae346b84cabc156cb217f0',
-  measurementId: 'G-V3ZFT8K6BW'
+  apiKey: "AIzaSyAQRWkV63etf7t2GClB_U-XvEs2SdyJYKE",
+  authDomain: "splash-7e99f.firebaseapp.com",
+  projectId: "splash-7e99f",
+  storageBucket: "splash-7e99f.appspot.com",
+  messagingSenderId: "751418611409",
+  appId: "1:751418611409:web:1810681dd1482b41b217f0",
+  measurementId: "G-C4MKB9GNYW"
 }
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app: FirebaseApp = initializeApp(firebaseConfig)
 export const db: Firestore = getFirestore(app)
+
+const auth: Auth = getAuth(app);
+const ui = new firebaseui.auth.AuthUI(auth);
 // storage = getStorage(app)
